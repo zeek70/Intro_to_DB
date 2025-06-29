86ccf693f59999ffb33170148defb1f22d9f56c3
zeek70 <105075451+zeek70@users.noreply.github.com>
2025-06-29 19:58:06 +0100

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,2 +1,2 @@
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
